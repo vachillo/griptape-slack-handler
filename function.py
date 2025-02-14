c3157772f4f6fdfb750b57efe1e903c380013c23
matt <matt@griptape.ai>
2025-02-14 17:03:00 -0600

    raw body

--- function.py
+++ function.py
@@ -1,4 +1,4 @@
 from griptape_slack_handler.slack_handler import handle_slack_event
 
-def slack_function(body, headers):
-    return handle_slack_event(body, headers)
+def slack_function(raw_body, headers):
+    return handle_slack_event(raw_body, headers)
